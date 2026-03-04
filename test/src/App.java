e413392ad0db01bbe747f1d0af11bf1bc1c9c2af
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-04 00:10:01 -0300

    alterando o nome da instancia

--- test/src/App.java
+++ test/src/App.java
@@ -1,13 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
- 
+
+        conta conta2 = new conta();
+
+        conta2.titular = "Gustavo";
+        conta2.numeroConta = 1234;
+        conta2.agencia = 10;
+        conta2.saldo = 1000;
+
+        conta2.depositar(500);
+        conta2.sacar(200);
+
+        conta2.mostrarSaldo();
     }
-
-    
-
-    
-
-
-
-    
 }
