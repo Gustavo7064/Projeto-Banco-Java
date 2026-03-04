e9e6f6a9837697f522bd9118ca6d6e6a5abdce99
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-03-04 00:01:31 -0300

    criando classes

---
+++ test/src/banco.java
@@ -0,0 +1,15 @@
+public class banco {
+
+    String nomeBanco;
+    int codigoBanco;
+    String endereco;
+    
+
+    public class Conta {
+
+    int numeroConta;
+    String titular;
+    double saldo;
+    int agencia;
+}
+}
